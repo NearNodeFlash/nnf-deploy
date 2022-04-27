961f9461ad743fab5f04abdec91c19ff7965e6cf
Dean Roehrich <dean.roehrich@hpe.com>
2022-04-27 08:44:50 -0500

    RABSW-877 Update craystack for SystemConfiguration resource (#18)
    Nnf-deploy will install the SystemConfiguration for craystack-lop.
    Nnf-deploy will do all labels and taints for craystack-lop.
    
    Refactor init.sh to consolidate common code.

--- init.sh
+++ init.sh
@@ -6,76 +6,66 @@
 
 source common.sh
 
-CMD=$1
+CMD="$1"
 
-if [ $# -eq 0 ]; then
-    echo "One of dp0 or dp1 is required"
-fi
-
-
-# The following commands apply to initializing the current DP0 environment
-# Nodes containing 'cn' are considered to be worker nodes for the time being.
-if [[ "$CMD" == dp0 ]]; then
+case "$CMD" in
+dp0)
+    # The following commands apply to initializing the current DP0 environment
+    # Nodes containing 'cn' are considered to be worker nodes for the time being.
     COMPUTE_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep cn | paste -d" " -s -)
     RABBIT_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -v cn | grep -v master | paste -d" " -s -)
     MASTER_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep master | paste -d" " -s -)
 
-    echo COMPUTE_NODES "$COMPUTE_NODES"
-    echo RABBIT_NODES "$RABBIT_NODES"
-    echo MASTER_NODES "$MASTER_NODES"
+    # We are using COMPUTE_NODES as generic k8s workers.
+    WORKER_NODES="$COMPUTE_NODES"
+    ;;
 
-    # Label the COMPUTE_NODES to allow them to handle wlm and nnf-sos
-    # We are using COMPUTE_NODES as generic k8s workers
-    for NODE in $COMPUTE_NODES; do
-        # Label them for SLCMs.
-        kubectl label node "$NODE" cray.nnf.manager=true
-        kubectl label node "$NODE" cray.wlm.manager=true
-    done
-
-    for NODE in $RABBIT_NODES; do
-        # Taint the rabbit nodes for the NLCMs, to keep any
-        # non-NLCM pods off of them.
-        kubectl taint node "$NODE" cray.nnf.node=true:NoSchedule
-
-        # Label the rabbit nodes for the NLCMs.
-        kubectl label node "$NODE" cray.nnf.node=true
-        kubectl label node "$NODE" cray.nnf.x-name="$NODE"
-    done
-
-    #Required for webhooks
-    install_cert_manager
-
-fi
-
-# The following commands apply to initializing the current DP1 environment
-# Nodes containing 'cn' are considered to be worker nodes for the time being.
-if [[ "$CMD" == dp1 ]]; then
+dp1)
+    # The following commands apply to initializing the current DP1 environment
     WORKER_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -i 'worker' | paste -d" " -s -)
     RABBIT_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -i 'node'   | grep -v master | paste -d" " -s -)
     MASTER_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -i 'master' | paste -d" " -s -)
 
-    echo WORKER_NODES "$WORKER_NODES"
-    echo RABBIT_NODES "$RABBIT_NODES"
-    echo MASTER_NODES "$MASTER_NODES"
+    ;;
 
-    # Label the WORKER_NODES to allow them to handle wlm and nnf-sos
-    for NODE in $WORKER_NODES; do
-        # Label them for SLCMs.
-        kubectl label node "$NODE" cray.nnf.manager=true
-        kubectl label node "$NODE" cray.wlm.manager=true
-    done
+craystack)
+    # The following commands apply to initializing the current Craystack-lop environment
+    RABBIT_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -i 'rabbit' | paste -d" " -s -)
+    MASTER_NODES=$(kubectl get nodes --no-headers -o custom-columns=:metadata.name | grep -i 'master' | paste -d" " -s -)
 
-    for NODE in $RABBIT_NODES; do
-        # Taint the rabbit nodes for the NLCMs, to keep any
-        # non-NLCM pods off of them.
-        kubectl taint node "$NODE" cray.nnf.node=true:NoSchedule
+    # We are using MASTER_NODES as generic k8s workers.
+    WORKER_NODES="$MASTER_NODES"
+    ;;
 
-        # Label the rabbit nodes for the NLCMs.
-        kubectl label node "$NODE" cray.nnf.node=true
-        kubectl label node "$NODE" cray.nnf.x-name="$NODE"
-    done
+*)
+    echo "Unknown system: $CMD"
+    exit 1
+    ;;
+esac
 
-    #Required for webhooks
-    install_cert_manager
+echo WORKER_NODES "$WORKER_NODES"
+echo RABBIT_NODES "$RABBIT_NODES"
+echo MASTER_NODES "$MASTER_NODES"
 
-fi
+# Label the WORKER_NODES to allow them to handle wlm and nnf-sos
+for NODE in $WORKER_NODES; do
+    # Label them for SLCMs.
+    kubectl label node "$NODE" cray.nnf.manager=true
+    kubectl label node "$NODE" cray.wlm.manager=true
+done
+
+for NODE in $RABBIT_NODES; do
+    # Taint the rabbit nodes for the NLCMs, to keep any
+    # non-NLCM pods off of them.
+    kubectl taint node "$NODE" cray.nnf.node=true:NoSchedule
+
+    # Label the rabbit nodes for the NLCMs.
+    kubectl label node "$NODE" cray.nnf.node=true
+    kubectl label node "$NODE" cray.nnf.x-name="$NODE"
+done
+
+#Required for webhooks
+install_cert_manager
+
+exit 0
+
