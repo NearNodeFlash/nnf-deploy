83b88864187155cb02435cea1b0bc86c78aa1533
Anthony Floeder <anthony.floeder@hpe.com>
2022-04-28 15:49:49 -0500

    RABSW-866: Insert LICENSE and copyright notices for HPE source code

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright 2021, 2022 Hewlett Packard Enterprise Development LP
+# Other additional copyright holders may be indicated within.
+#
+# The entirety of this work is licensed under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Build in each submodule with the submodule defaults. If you wish to use an overlay
 # see "hpc-rabsw-nnf-deploy make docker-build"; this will use the overlays defined
 # in the ./config/systems.yaml
--- kind.sh
+++ kind.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright 2021, 2022 Hewlett Packard Enterprise Development LP
+# Other additional copyright holders may be indicated within.
+#
+# The entirety of this work is licensed under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Various Kubernetes in Docker (KinD) related scripts
 
 source common.sh
@@ -39,7 +56,7 @@ EOF
     kind create cluster --wait 60s --image=kindest/node:v1.22.5 --config kind-config.yaml
 
     # TODO: Initialization should move to init.sh
-    
+
     # Use the kind-control-plane node for the SLCMs.  Remove its default taint
     # and label it for our use.
     kubectl taint node kind-control-plane node-role.kubernetes.io/master:NoSchedule-
