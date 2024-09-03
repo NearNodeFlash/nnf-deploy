af4043b03319d9bb018612efb7f2a1bc2eaba778
Dean Roehrich <dean.roehrich@hpe.com>
2024-09-03 10:50:51 -0500

    Use pipefail in release-all (#201)
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- tools/release-all/release-all.sh
+++ tools/release-all/release-all.sh
@@ -17,6 +17,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+set -o pipefail
+
 PROG=$(basename "$0")
 
 # Crude associative-array-ish thingy that works in bash v3 for Mac.
